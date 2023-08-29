9cb810194ce6708732ea3f7b386beae1604be754
TatjanaWas <123033240+tanjawas@users.noreply.github.com>
2023-08-30 01:13:20 +0200

    änderung

--- client/src/components/Facts.jsx
+++ client/src/components/Facts.jsx
@@ -21,7 +21,62 @@ const Facts = () => {
             <li>women felt they have no physical skills to define themselves</li>
             </ul>
         </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+          <div>
+            <h3>Welcome to TravelHer:<br></br> 
+                Stories from Adventurous Women</h3>
+                <br></br>
+          </div>
+              <div className='invitation'>
+                <span>
+                Traveling is not just about exploring the world; it's also a journey to discover oneself.<br></br>Our website is the safe haven for all the fearless women out there who love to venture out into the world on their own. <br></br><br></br>Here, we aim to collect stories from women who have embarked on solo journeys to experience adventures, explore cultures, and empower themselves.<br></br>
+              </span>
+              </div>
+                <br></br>
+                    <br></br>
+                    <div class="container">
+        <div class="element">
+            <p class="initial-text">Why travel alone?</p>
+            <p class="additional-text">Traveling alone is a transformative experience that can expand your horizons and strengthen your independence. It allows you to make your own decisions, forge new friendships, and experience moments of self-reflection that are only possible on such journeys</p>
+        </div>
+        <div class="element">
+            <p class="initial-text">Our Diversity</p>
+            <p class="additional-text">Whether you're an experienced adventurer or just starting to explore the world – there's a place for you here. We welcome women of all ages, ethnic backgrounds, and experience levels. Our community is diverse, just like the places we explore</p>
+        </div>
+        <div class="element">
+            <p class="initial-text">What We're Looking For</p>
+            <p class="additional-text">We encourage women to share their stories and experiences on our pages. This could be a travel diary from your recent adventure, a guide to safe solo travel, or a deep dive into the cultural experiences you've had. We believe that every story is inspiring and can encourage other women to embark on their own journeys.</p>
+        </div>
+    </div>
+        <br></br>
+    <div class="container">
+    <div class="element">
+            <p class="initial-text">Share Your Contribution</p>
+            <p class="additional-text">Would you like to share your travel story?<br></br> It's easy! Click on "Submit a Post" and upload your article.<br></br>Our editorial team will review it and provide feedback before it's published. We want to ensure that every story gets the attention it deserves</p>
+        </div> 
+        <div class="element">
+            <p class="initial-text">Our Vision</p>
+            <p class="additional-text">Our vision is to empower women to explore the world, building confidence, tolerance, and cultural understanding along the way.<br></br>Traveling alone can be a life-changing experience, and we're here to support women on that journey.</p>
+        </div> 
+        <div class="element">
+            <p class="initial-text">Come, Join Us</p>
+            <p class="additional-text">We invite you to become a part of our community and be inspired by the stories and adventures shared here. <br></br>Whether you're just starting out or you're already an experienced solo traveler, you're welcome here.</p>
+        </div> 
+        
+    </div>
+    <br></br>
+    <br></br>
+    <br></br>
+          <div class="container2">
+          <div class="goodbye">
 
+<p>Join us on this exciting journey as we celebrate the world, ourselves, and the power of solo travel!<br></br></p>
+            </div>       
+            </div>
     </div>
   )
 }

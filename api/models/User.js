cf4bd25a0c687bd84132bdd20726b78324a4eae6
karen-karyen-lky <132813271+karen-karyen-lky@users.noreply.github.com>
2023-09-10 14:01:09 +0200

    Einpacke FrontEnd und Backend
    - Styling Landing Page verbeseern
    - Backend + Frontend zusammen

---
+++ api/models/User.js
@@ -0,0 +1,42 @@
+//const mongoose = require("mongoose")
+//do not use the code outcommended above with require, instead use the first code line with import
+//export all Schemas by default and not module.export, use curly brackets for more than one Schema without command default if you need it
+// in each step you want to import models (auth.js/post.js,...) from models folder to your controller or router files just use the schema as "UserSchema" as the constant you want to import
+
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        reqired: true,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+    },
+    { timestamps:true }
+);
+
+// const LocationSchema = new mongoose.Schema({
+//     location:{
+//         type:String,
+//         required: true,
+//         unique: true
+//     },
+//     },
+//     { timestamps:true }
+// );
+const User = mongoose.model("User", UserSchema);
+export default User;
+//export { UserSchema, LocationSchema }

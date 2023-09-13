cc8b8efd0664c979f13c662d1b9bb9825fd0a8da
glsmknk <gkinik@outlook.de>
2023-09-13 22:12:00 +0200

    Write wurde für frontend Publish funktion eingebaut und funktipniert. style muss angepasst werden

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 // import { useLocation, useNavigate } from "react-router-dom";
 // import moment from "moment";
 import { Context } from "../context/Context.js";
-import "./write.css";
+//import "./write.css";
 
 export default function Write() {
   const [title, setTitle] = useState("");
@@ -80,46 +80,47 @@ export default function Write() {
       console.log(err);
     }
   }; */
+  
 //Style ab hier
 
-  return (
-    <div className="write">
-      {file && (
-        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
-      )}
-      <form className="writeForm" onSubmit={handleSubmit}>
-        <div className="writeFormGroup">
-          <label htmlFor="fileInput">
-            <i className="writeIcon fas fa-plus"></i>
-          </label>
-          <input
-            type="file"
-            id="fileInput"
-            style={{ display: "none" }}
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <input
-            type="text"
-            placeholder="Title"
-            className="writeInput"
-            autoFocus={true}
-            onChange={e=>setTitle(e.target.value)}
-          />
-        </div>
-        <div className="writeFormGroup">
-          <textarea
-            placeholder="Tell your story..."
-            type="text"
-            className="writeInput writeText"
-            onChange={e=>setDesc(e.target.value)}
-          ></textarea>
-        </div>
-        <button className="writeSubmit" type="submit">
-          Publish
-        </button>
-      </form>
-    </div>
-  );
+return (
+  <div className="write">
+    {file && (
+      <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+    )}
+    <form className="writeForm" onSubmit={handleSubmit}>
+      <div className="writeFormGroup">
+        <label htmlFor="fileInput">
+          <i className="writeIcon fas fa-plus"></i>
+        </label>
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: "none" }}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <input
+          type="text"
+          placeholder="Title"
+          className="writeInput"
+          autoFocus={true}
+          onChange={e=>setTitle(e.target.value)}
+        />
+      </div>
+      <div className="writeFormGroup">
+        <textarea
+          placeholder="Tell your story..."
+          type="text"
+          className="writeInput writeText"
+          onChange={e=>setDesc(e.target.value)}
+        ></textarea>
+      </div>
+      <button className="writeSubmit" type="submit">
+        Publish
+      </button>
+    </form>
+  </div>
+);
 }
 
 /*  return (

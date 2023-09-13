b3bd580db2e60cd5034562c4da95a7746944a349
glsmknk <gkinik@outlook.de>
2023-09-13 21:52:17 +0200

    Publish funktioniert in Write, Style muss noch adaptiert werden an Haupt style.css

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from "react";
-import ReactQuill from "react-quill";
+import {  useContext, useState } from "react";
+// import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import axios from "axios";
-import { useLocation, useNavigate } from "react-router-dom";
-import moment from "moment";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import moment from "moment";
+import { Context } from "../context/Context.js";
+import "./write.css";
 
-const Write = () => {
+export default function Write() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc,
+    };
+    if (file) {
+      const data =new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.photo = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.post("/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
+    } catch (err) {}
+  };
+
+/*const Write = () => {
   const state = useLocation().state;
   const [value, setValue] = useState(state?.title || "");
   const [title, setTitle] = useState(state?.desc || "");
@@ -48,9 +79,50 @@ const Write = () => {
     } catch (err) {
       console.log(err);
     }
-  };
+  }; */
+//Style ab hier
 
   return (
+    <div className="write">
+      {file && (
+        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+      )}
+      <form className="writeForm" onSubmit={handleSubmit}>
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+            onChange={e=>setTitle(e.target.value)}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+            onChange={e=>setDesc(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
+      </form>
+    </div>
+  );
+}
+
+/*  return (
     <div className="add">
       <div className="content">
         <input
@@ -141,6 +213,6 @@ const Write = () => {
       </div>
     </div>
   );
-};
+}; 
 
-export default Write;
+export default Write; */
